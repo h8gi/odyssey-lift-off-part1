7bca535d6ad49760ecd994d0ef14cb3768a7df3f
Hiroki Yagi <hiro.yagi0245@gmail.com>
2022-07-09 18:13:18 +0900

    add server

---
+++ server/src/schema.js
@@ -0,0 +1,28 @@
+const { gql } = require('apollo-server')
+
+const typeDefs = gql`
+type Query {
+  "Query to get tracks array for the homepage grid"
+  tracksForHome: [Track!]!
+}
+
+"A track is a group of Modules that teaches about a specific topic"
+type Track {
+  id: ID!
+  title: String!
+  author: Author!
+  thumbnail: String
+  length: Int
+  modulesCount: Int
+}
+
+"Author of a complete Track"
+type Author {
+  id: ID!
+  name: String!
+  photo: String
+}
+
+`
+
+modeule.exports = typeDefs
